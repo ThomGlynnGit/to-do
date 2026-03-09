1f12897662e9b79f5e94a4eee5867d6aaeaa4712
ThomGlynnGit <98777656+ThomGlynnGit@users.noreply.github.com>
2026-03-09 11:17:36 +0000

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+// webpack.config.js
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    entry: {
+        app: "./src/index.js",
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/index.html"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "production",
+            template: "./src/index.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
