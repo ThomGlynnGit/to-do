0c6a65c02dac726c672b858d8cb0190089d99935
Thomas Taylor <98777656+ThomGlynnGit@users.noreply.github.com>
2026-03-13 13:42:37 +0000

    Create getters and setters for task class

--- src/scripts/task.js
+++ src/scripts/task.js
@@ -1,3 +1,5 @@
+import { isDate, isFuture, isValid, lightFormat } from "date-fns"
+
 class Task {
     constructor(title, description, dueDate, priority, notes){
         this.title = title;
@@ -8,6 +10,7 @@ class Task {
         this.checked = false;
     }
 
+    //getters and setters - validation is completed in setters
     get title(){
         return this._title;
     }
@@ -23,4 +26,76 @@ class Task {
             this._title = name;
         }
     }
+
+    get description(){
+        return this._description
+    }
+
+    set description(info){
+        if(info.length > 50){
+            return "long"
+        }
+        else{
+            this._description = info;
+        }
+    }
+
+    get dueDate(){
+        return this._dueDate
+    }
+
+    set dueDate(date){
+        if(!isDate(date)){
+            return "notDate"
+        }
+        else if(!isFuture(date)){
+            return "past"
+        }
+        else if(!isValid(date)){
+            return "invalid"
+        }
+        else{
+            this._dueDate = lightFormat(date,"dd-MM-yy");
+        }
+    }
+    
+    get priority(){
+        return this._priority
+    }
+
+    set priority(num){
+        if(num === NaN || Number.isInteger(num)){
+            return "notNum"
+        }
+        else{
+            this._priority = num;
+        }
+    }
+
+    get notes(){
+        return this._notes
+    }
+
+    set notes(text){
+        if(text.length > 140){
+            return "long"
+        }
+        else{
+            this._notes = text;
+        }
+    }
+
+    get checked(){
+        return this._checked
+    }
+
+    set checked(bool){
+        if(typeof bool !== "boolean"){
+            return "notBool"
+        }
+        else{
+            this._checked = bool;
+        }
+    }
+
 }
